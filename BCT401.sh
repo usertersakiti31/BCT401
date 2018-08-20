d5a765a3165540de8d5ef0b383820503ac4422e2
usertersakiti31 <42396277+usertersakiti31@users.noreply.github.com>
2018-08-20 21:31:54 +0200

    BCT401.sh

---
+++ BCT401.sh
@@ -0,0 +1,206 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "[+] Tools BCT [+]"
+sleep 1
+echo "\033[33;1m# Author:./WhiteSystem404"
+sleep 1
+echo "\033[34;1m# Contack:dedidarmadi745@gmail.com"
+sleep 1
+echo "\033[33;1m# Thanks To: Member Banyumas Cyber Team"
+sleep 1
+echo "\033[35;1m[+] Tools Yang Tersedia [+]"
+echo "(1)  JDID"
+echo "(2)  Telkomsel"
+echo "(3)  PHD"
+echo "(4)  TOKOPEDIA"
+echo "(5)  LAZADA"
+echo "(6)  hooq"
+echo "(7)  kfc"
+echo "(8)  Matahari Mall"
+echo "(9)  WhisKas"
+echo "(10) Pencari CC buat Carder Wibu"
+echo "(11) Dengerin Lagu Yt di Termux"
+echo "(12) install sqlscan"
+echo "(13) install hash-buster"
+echo "(14) install Red_Hawk"
+echo "(15) AutoLike Facebook"
+echo "\033[34;1m(999). Info"
+echo "(00) OUT"
+echo "\033[33;1m root >>#@× | BCT ===}}"
+read mrrm
+if [ $mrrm = 01 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[32;1m"
+figlet "JDID"
+php jdid.php
+fi
+
+if
+[ $mrrm = 02 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "Call Prank"
+php telkomsel.php
+fi
+
+if [ $mrrm = 03 ] || [ $mrrm = 3 ]
+then
+clear
+toilet -f mono12 -F gay "P H D"
+figlet "PHD"
+php phd.php
+fi
+
+
+if [ $mrrm = 04 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f mono12 -F gay "TOKOPEDIA"
+echo "\033[32;1m"
+php tokped.php
+fi
+
+if
+[ $mrrm = 05 ] || [ $mrrm = 5 ]
+then
+clear
+toilet -f mono12 -F gay "LAZADA"
+echo "\033[31;1m"
+python2 lazada.py
+fi
+
+if
+[ $mrrm = 06 ] || [ $mrrm = 6 ]
+then
+clear
+toilet -f standard -F gay "hooq"
+echo "\033[32;1m"
+php hooq.php
+fi
+
+if
+[ $mrrm = 07 ] || [ $mrrm = 7 ]
+then
+clear
+toilet -f standard -F gay "kfc"
+echo "\033[32;1m"
+php kfc.php
+fi
+
+if
+[ $mrrm = 08 ] || [ $mrrm = 8 ]
+then
+clear
+toilet -f standard -F gay "Matahari Mall"
+echo "\033[32;1m"
+php mataharimall.php
+fi
+
+if
+[ $mrrm = 09 ] || [ $mrrm = 9 ]
+then
+clear
+toilet -f standard -F gay "WhisKas Spam"
+echo "\033[32;1m"
+php whiskas.php
+fi
+
+if
+[ $mrrm = 010 ] || [ $mrrm = 10 ]
+then
+clear
+toilet -f standard -F gay "White System./404"
+echo "\033[32;1m"
+php key.php
+fi
+
+if 
+[ $mmrm = 011 ] || [ $mmrm = 11 ]
+then
+clear
+cowsay -f kiss.cow "Banyumas Cyber Team" | lolcat
+figlet "./WhiteSystem404" | lolcat
+sleep 1
+apt update && apt upgrade
+apt install pip
+pkg install python
+pkg install python2
+pip install mps_youtube
+pip install youtube_dl
+pkg install mpv
+mpsyt
+fi
+
+if [ $mmrm = 012 ] || [ mmrm = 12 ]
+then
+clear
+toilet -f slant -F gay "Banyumas Cyber Team" 
+pkg install update && pkg install upgrade
+pkg install git
+pkg install php
+php sqlscan.php
+fi
+
+if 
+[ $mmrm = 013 ] || [ $mmrm = 13 ]
+then
+clear
+toilet -f mono12 -F gay "Banyumas Cyber Team" 
+apt upgrade
+apt install python2
+apt install git
+git clone https://github.com/UltimateHackers/Hash-Buster
+mv Hash-Buster $HOME
+cd $HOME/Hash-Buster
+python2 hash.py
+fi
+
+if 
+[ $mmrm = 014 ] || [ $mmrm = 14 ]
+then
+clear
+toilet -f standard -F gay "Banyumas Cyber Team"
+sleep 3
+apt update
+apt install git
+apt install php
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+mv RED_HAWK $HOME
+cd $HOME/RED_HAWK
+chmod +x rhawk.php
+php rhawk.php
+fi
+
+if 
+[ $mmrm = 015 ] || [ $mmrm = 15 ]
+then
+clear
+toilet "Banyumas Cyber Team"
+cd module
+php autolike.php
+fi
+
+if
+[ $mrrm = 0999 ] || [ $mrrm = 999 ]
+then
+clear
+toilet -f slant --gay "[+] Tools BCT [+]"
+echo "\033[36;1mAuthor : ./WhiteSystem404"
+echo "\033[34;1mContack :dedidarmadi745@gmail.com
+sleep 1
+echo "\033[30;1m tunggu 8 detik"
+sleep 8
+sh BCT34.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Sampai berjumpa lagi and jangan lupa bahagia"
+sleep 1
+fi
+
